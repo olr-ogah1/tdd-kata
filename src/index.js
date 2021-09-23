499eb89a71cac62e29c11a0e7dd4465f85d55551
Onyela Ogah <onyela.ogah@gmail.com>
2021-09-23 19:22:21 +0100

    Solve FizzBuzz

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-export const projectName = () => {
-  return 'Kata starter project';
-};
+export const FizzBuzzer = (number) => 
+  number % 15 === 0 ? 'FizzBuzz' : 
+  number % 5 === 0 ? 'Buzz' : 
+  number % 3 === 0 ? 'Fizz' : 
+  number;
+
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,22 @@
-import {projectName} from "./index";
+import {FizzBuzzer} from "./index";
 
-describe('Starter project should', () => {
-  it('output its name', () => {
-    expect(projectName()).toEqual('Kata starter project');
+
+describe('FizzBuzzer function should', () => {
+  it('return 1 given 1', () => {
+    const actual = FizzBuzzer(1);
+    expect(actual).toEqual(1);
   });
+  it('return Fizz when number is multiple of 3', () => {
+    const actual = FizzBuzzer(6);
+    expect(actual).toEqual('Fizz')
+  });
+  it('return Buzz when number is multiple of 5', () => {
+    const actual = FizzBuzzer(10);
+    expect(actual).toEqual('Buzz')
+  })
+  it('return FizzBuzz when number is multiple of 15', () => {
+    const actual = FizzBuzzer(15);
+    expect(actual).toEqual('FizzBuzz')
+  })
+
 });
